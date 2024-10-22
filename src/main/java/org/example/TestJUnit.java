38e8b254e018ac4bc4b12e089fb3d6b75dfe81f7
Simon Kollmannsberger <simon@kollmannsberger.de>
2024-10-22 08:45:20 +0200

    Adding DatabaseConnection

--- src/main/java/org/example/TestJUnit.java
+++ src/main/java/org/example/TestJUnit.java
@@ -1,14 +1,26 @@
-/*package org.example;
+package org.example;
 
-import org.junit.Test;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.AssertJUnit.fail;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class TestJUnit {
 
     @Test
-    public void test(){
-        assertTrue(true);
+    public void testDatabaseConnection() {
+        String url = "jdbc:mariadb://localhost:3306/schulprojektdb";
+        String user = "root";
+        String password = "#Sim06Kolli-workspace";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            // Test: Verbindung sollte nicht null sein
+            assertNotNull(connection, "Die Verbindung sollte nicht null sein.");
+        } catch (SQLException e) {
+            fail("Fehler bei der Verbindung zur MariaDB: " + e.getMessage());
+        }
     }
 }
-*/
